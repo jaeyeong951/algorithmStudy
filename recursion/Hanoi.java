d33f118427aa9ab2aa48e2679de1dfcd2727bef3
Jaeyeong <201524443@pusan.ac.kr>
2020-05-27 16:34:44 +0900

    알고지름 개념별로 정리할 예정

---
+++ recursion/Hanoi.java
@@ -0,0 +1,53 @@
+import java.io.*;
+
+/*
+기본적인 재귀 알고리즘을 활용한 문제입니다.
+ */
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static int input;
+    static int count;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        input = Integer.parseInt(br.readLine());
+        count = 0;
+
+        hanoi(input, 1, 3);
+        sb.insert(0,"\n");
+        sb.insert(0,count);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    static void hanoi(int num, int from, int to){
+        if(num == 1){
+            count++;
+            sb.append(from);
+            sb.append(" ");
+            sb.append(to);
+            sb.append("\n");
+        }
+        else{
+            if(from+to == 3){
+                hanoi(num-1, from, 3);
+                hanoi(1, from, to);
+                hanoi(num-1, 3, to);
+            }
+            else if(from+to == 4){
+                hanoi(num-1, from, 2);
+                hanoi(1, from, to);
+                hanoi(num-1, 2, to);
+            }
+            else if(from+to == 5){
+                hanoi(num-1, from, 1);
+                hanoi(1, from, to);
+                hanoi(num-1, 1, to);
+            }
+        }
+    }
+}
